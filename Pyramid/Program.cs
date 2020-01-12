3a5dd091d828acfe15c786ea4504f2161588265b
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-01-12 15:25:57 -0800

    first commit

---
+++ Pyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int y = 1; y <= 2 * i - 1; y += 1)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine("");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my draw a pyramid console application");
+            Console.Write("please enter a Number:");
+            int userinput = Convert.ToInt32(Console.ReadLine());
+            DrawPyramid(userinput); 
+        }
+    }
+}
